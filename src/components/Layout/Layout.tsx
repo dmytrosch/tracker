b68f5c8781877654cd8da4159404f2f89de61676
Dmytro Shcherbonos <dimaus.sch@gmail.com>
2024-02-23 16:43:45 +0200

    ThemeSwitcher component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Layout.module.css';
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 
 interface IProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ const Layout: React.FC<IProps> = ({ children }) => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
+        <ThemeSwitcher isDark onChange={(e) => {}} />
         <h1 className={styles.title}>tracker</h1>
       </header>
       <main>{children}</main>
